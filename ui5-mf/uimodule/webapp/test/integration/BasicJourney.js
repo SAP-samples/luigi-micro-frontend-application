51af2e13505b8533c1da33a26a064dbb9b1dd045
Aleksandra Simeonova <aleksandra.simeonova@sap.com>
2020-10-25 09:28:45 +0100

    migrate changes from SAP Samples staging repository

---
+++ ui5-mf/uimodule/webapp/test/integration/BasicJourney.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+  "sap/ui/test/opaQunit",
+  "luigi/ui5/test/integration/pages/Order"
+], function (opaTest) {
+  "use strict";
+
+  opaTest("should show correct number of nested pages", function (Given, When, Then) {
+
+    // Arrangements
+    Given.iStartMyApp();
+
+    // Assertions
+    Then.onTheAppPage.iShouldSeePageCount(1);
+
+    // Cleanup
+    Then.iTeardownMyApp();
+  });
+
+});
