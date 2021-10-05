9d16c08c3c2f4f75abac558bb39d785e1dcb4273
viktorsperling <viktor.sperling@sap.com>
2021-10-05 11:37:51 +0200

    Initial commit on updated tutorial

--- react-core-mf/src/index.js
+++ react-core-mf/src/index.js
@@ -1,39 +1,17 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { addInitListener, addContextUpdateListener, uxManager } from '@luigi-project/client';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Home } from './views/Home.jsx';
-import { Products } from './views/Products.jsx';
-import { ProductDetail } from './views/ProductDetail.jsx';
-import { dict } from './language.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-class App extends Component {
- constructor(props) {
-   super(props);
-   this.state = { currentLocale : 'en-US' };
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-   const updateCurrentLanguage = () =>{
-      this.setState({
-        currentLocale: uxManager().getCurrentLocale()
-      });
-   };
-   addInitListener(() => {
-     console.log('Luigi Client initialized.');
-     updateCurrentLanguage();
-   });
-   addContextUpdateListener(()=>{
-      updateCurrentLanguage();
-   });
- }
- render() {
-   return (
-     <Router basename={`/app.html#`}>
-       <Route path="/home" render={(props) => <Home {...props} localeDict={dict[this.state.currentLocale]} currentLocale={this.state.currentLocale} />} />
-       <Route path="/products" render={(props) => <Products {...props} localeDict={dict[this.state.currentLocale]} />} />
-       <Route path="/productDetail/:id" render={(props) => <ProductDetail {...props} localeDict={dict[this.state.currentLocale]} />} />
-     </Router>
-   );
- }
-}
- 
-render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
